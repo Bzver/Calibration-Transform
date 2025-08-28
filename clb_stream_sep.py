5ca80dee4092c3c6881982fa506a4443c69ca0dc
Bzver <t.c.pedrovsky@gmail.com>
2025-08-28 11:19:37 +0800

    A wrapper script dedicated to stream separation only

---
+++ clb_stream_sep.py
@@ -0,0 +1,27 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+import utils.clb_separator as cbs
+
+def folder_select_dialog():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main window
+    folder_path = filedialog.askdirectory()  # Open folder selection dialog
+    return folder_path
+
+def collect_all_videos_in_folder(folder_path):
+    video_files = []
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            if file.lower().endswith(('.mp4', '.avi', '.mov', '.mkv')):
+                video_files.append(os.path.join(root, file))
+    return video_files
+
+if __name__ == "__main__":
+    folder_path = folder_select_dialog()
+    video_files = collect_all_videos_in_folder(folder_path)
+    for video_file in video_files:
+        output_dir = os.path.join(folder_path, f"video_file")
+        os.makedirs(output_dir, exist_ok=True)
+        cbs.separate_video_stream(num_view=4, video_filepath=video_file, project_dir=output_dir, mode="calibration")
+
